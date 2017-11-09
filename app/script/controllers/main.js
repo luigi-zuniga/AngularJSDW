814c759de563e109bd1b0140e0421690dffa43f3
luigi-zuniga <luis.alberto.zuniga@outlook.com>
2017-11-09 06:16:16 -0300

    Ejemplo Angular con Variante opcion 2 controller

--- app/script/controllers/main.js
+++ app/script/controllers/main.js
@@ -1,5 +1,18 @@
 angular
-.module('pruebaApp',[])
-.controller('pruebaCtrl',function(){
-    this.algo = "Eso funciona , opcion 2";
-});
+.module('acumuladorApp',[])
+.controller("acumuladorCtrl",controladorPrincipal);
+
+function controladorPrincipal(){
+    //esta funcion es mi controlador
+    var scope = this;
+    scope.total=0;
+    scope.cuanto=0;
+
+    scope.sumar = function(){
+        scope.total+=parseInt(scope.cuanto);
+    }
+
+    scope.restar=function(){
+        scope.total-=parseInt(scope.cuanto);
+    }
+};
